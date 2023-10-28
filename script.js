83b6f03b30b9f82ec2201d14f0c8b62d584870b6
Sameep1907 <122293329+sameep19@users.noreply.github.com>
2023-10-28 17:00:16 -0400

    files pushed

--- script.js
+++ script.js
@@ -1,8 +1,56 @@
-$(document).ready(function(){
-	$('#yearly-salary, #percent').on('keyup', ()=>{
-		var salary = $("#yearly-salary").val();
-		var percentage = $("#percent").val();
-		let amountToSpend = Number(salary) * Number(percentage) * 0.01;
-		$('#amount').text(`$${amountToSpend.toFixed(2)}`);
-	});
+/*  Sameep Pradeep
+    Assignment 05
+*/  
+
+$(document).ready(function () {
+
+    // Define the ContentItem class
+class ContentItem {
+    constructor(id, name, description, categoryGenre) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.categoryGenre = categoryGenre;
+    }
+
+    updateContentItem(id, name, description, categoryGenre) {
+        if (this.id === id) {
+            if (name !== null) this.name = name;
+            if (description !== null) this.description = description;
+            if (categoryGenre !== null) this.categoryGenre = categoryGenre;
+        }
+    }
+
+    toString() {
+        return `<div class="content-item-wrapper" id="content-item-${this.id}">
+                    <h2>${this.name}</h2>
+                    <p>${this.description}</p>
+                    <div>${this.categoryGenre}</div>
+                </div>`;
+    }
+}
+
+// array of 5 items
+let contentItems = [
+    new ContentItem(0, 'iPhone6', 'The 2014 iPhone with a sleek design and 4.7-inch display.', 'iOS 13'),
+    new ContentItem(1, 'iPhone8', 'A 2017 iPhone featuring wireless charging and a powerful A11 Bionic chip', 'iOS 14'),
+    new ContentItem(2, 'iPhone11', 'Introduced in 2019, it brought dual-camera capabilities and powerful performance.', 'iOS 15'),
+    new ContentItem(3, 'iPhone14', 'It runs on Apples A15 Bionic processor, which is better equipped to handle newer iOS features.', 'iOS 16'),
+    new ContentItem(4, 'iPhone15', 'Apple includes its A16 Bionic processor in its latest phone, which gives you faster everyday performance', 'iOS 17'),
+];
+
+// Use jQuery to add each content item to the page
+
+    contentItems.forEach(function(item) {
+        $('#content-item-list').append(item.toString());
+    });
+
+    // CSS
+    $('.content-item-wrapper').css({
+        
+        'padding': '12px',
+        'width': '250px',
+        'margin': '0 auto 10px auto',
+        'border': '3px solid black'
+    });
 });
