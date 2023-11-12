89f3e2f542d1153208a4e638e0f2e8debeb900ca
Sameep1907 <122293329+sameep19@users.noreply.github.com>
2023-11-11 20:17:34 -0500

    Files Uploaded for asgn 7

--- js/jquery.js
+++ js/jquery.js
@@ -9,7 +9,9 @@
  * Released under the MIT license
  * https://jquery.org/license
  *
- * Date: 2021-03-02T17:08Z
+ * Date: 2022-02-20T13:00Z
+ * 
+ * 
  */
 (function (global, factory) {
 
@@ -17,13 +19,7 @@
 
 	if (typeof module === "object" && typeof module.exports === "object") {
 
-		// For CommonJS and CommonJS-like environments where a proper `window`
-		// is present, execute the factory and get jQuery.
-		// For environments that do not have a `window` with a `document`
-		// (such as Node.js), expose a factory as module.exports.
-		// This accentuates the need for the creation of a real `window`.
-		// e.g. var jQuery = require("jquery")(window);
-		// See ticket #14549 for more info.
+	
 		module.exports = global.document ?
 			factory(global, true) :
 			function (w) {
@@ -39,11 +35,7 @@
 	// Pass this if window is not defined yet
 })(typeof window !== "undefined" ? window : this, function (window, noGlobal) {
 
-	// Edge <= 12 - 13+, Firefox <=18 - 45+, IE 10 - 11, Safari 5.1 - 9+, iOS 6 - 9.1
-	// throw exceptions when non-strict code (e.g., ASP.NET 4.5) accesses strict mode
-	// arguments.callee.caller (trac-13335). But as of jQuery 3.0 (2016), strict mode should be common
-	// enough that all such attempts are guarded in a try block.
-	"use strict";
+
 
 	var arr = [];
 
@@ -76,13 +68,7 @@
 
 	var isFunction = function isFunction(obj) {
 
-		// Support: Chrome <=57, Firefox <=52
-		// In some browsers, typeof returns "function" for HTML <object> elements
-		// (i.e., `typeof document.createElement( "object" ) === "function"`).
-		// We don't want to classify *any* DOM node as a function.
-		// Support: QtWeb <=3.8.5, WebKit <=534.34, wkhtmltopdf tool <=0.12.5
-		// Plus for old WebKit, typeof returns "function" for HTML collections
-		// (e.g., `typeof document.getElementsByTagName("div") === "function"`). (gh-4756)
+	
 		return typeof obj === "function" && typeof obj.nodeType !== "number" &&
 			typeof obj.item !== "function";
 	};
@@ -114,16 +100,7 @@
 		if (node) {
 			for (i in preservedScriptAttributes) {
 
-				// Support: Firefox 64+, Edge 18+
-				// Some browsers don't support the "nonce" property on scripts.
-				// On the other hand, just using `getAttribute` is not enough as
-				// the `nonce` attribute is reset to an empty string whenever it
-				// becomes browsing-context connected.
-				// See https://github.com/whatwg/html/issues/2369
-				// See https://html.spec.whatwg.org/#nonce-attributes
-				// The `node.getAttribute` check was added for the sake of
-				// `jQuery.globalEval` so that it can fake a nonce-containing node
-				// via an object.
+		
 				val = node[i] || node.getAttribute && node.getAttribute(i);
 				if (val) {
 					script.setAttribute(i, val);
@@ -144,9 +121,6 @@
 			class2type[toString.call(obj)] || "object" :
 			typeof obj;
 	}
-	/* global Symbol */
-	// Defining this global in .eslintrc.json would create a danger of using the global
-	// unguarded in another place, it seems safer to define global only for this module
 
 
 
